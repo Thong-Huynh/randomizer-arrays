0acc6722e4fe9d59d621dd2ef52e6e34fdb29afe
Thong-Huynh <minhthong.huynh@sjsu.edu>
2021-03-16 02:31:50 -0700

    randomizer-arrays

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// reindeers
+let reindeers = ["Rudolph","Zack","Lala","Nino","Max","Punch"];
+
+function setup() {
+  createCanvas(600, 600);
+  background(220);
+
+  reindeers[5] = "Punchy POP"
+  console.log(reindeers);
+
+}
+
+function draw() {
+
+
+}
